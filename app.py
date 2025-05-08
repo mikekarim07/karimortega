0f6a09c2263622fd2103979ad0beaadb07f894de
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2025-05-07 20:04:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 st.title("Hola mi amigo Tony")
-st.button("Push de red button Laika")
+# st.button("Push de red button Laika")
 
-if button:
+if st.button("Push de red button Laika")
   st.write("Yeah baby")
